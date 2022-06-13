4fc29f01884846f924a118af95f656cc83091fdf
romashaa <mariia.romanenko@ukma.edu.ua>
2022-06-13 19:16:50 +0300

    Initial commit

---
+++ src/main/java/com/example/hotel_admin/HotelAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotel_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hotel_admin/HotelAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotel_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
